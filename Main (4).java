3681457d5d5d3ef7ba727065a22f42ccf37a4adb
Helenapati <88396481+Helenapati@users.noreply.github.com>
2021-11-03 21:53:45 -0300

    Helena Patrícia de Souza Silva - 2ºA

---
+++ Main (4).java
@@ -0,0 +1,68 @@
+public class Banco {
+    
+    public static void main (String{} args)
+    ContaCorrente cb = new ContaCorrente();
+    ContaPoupanca cp= new ContaPoupanca();
+
+     public class ContaCorrente{
+        private int saldo; 
+         
+        public ContaCorrente(int saldo){
+            this.ContaCorrente= 1000;
+            
+         System.out.println("Valor total é:"+ ContaCorrente);
+         
+        }
+      public class Deposito extends ContaCorrente{
+          public Deposito(int saldo){
+              int saldo= 100;
+             
+              System.out.println("Valor :"+public Deposito);
+          }
+      }
+     public class Deposito extends ContaCorrente{
+          public Deposito(int saldo){
+              int saldo= 400;
+             
+              System.out.println("Valor é:"+public Deposito);
+             
+          }    
+         public class Saque extends ContaCorrente{
+             public Saque (int saldo){
+                 int saldo - 200; 
+             }
+         }
+     
+         
+     }
+          public class ContaPoupanca{
+            private int saldo; 
+         
+         public ContaPoupanca(int saldo){
+              this.ContaPoupancae= 5000;
+            
+               System.out.println("Valor total é:"+ ContaPoupanca);
+       
+            }
+       
+         public class Deposito extends ContaPoupanca{
+          public Deposito(int saldo){
+              int saldo= 1000;
+             
+              System.out.println("Valor :"+public Deposito);
+          }
+        
+              }
+           public class Juros extends ContaPoupanca{
+               public Juros (int saldo){
+                   int= int saldo ;
+                   flout i= 0.5; 
+                   x= (int saldo(i/100));
+                   System.out.println("O valor total é de:"+x); 
+               }
+           }
+     }
+     
+     
+
+
